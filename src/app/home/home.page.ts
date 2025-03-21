8edc767bc9f3fe04920065ac1710ba41aec4fde0
CaioGotardo <caigotardo.dev@gmail.com>
2025-03-21 10:01:19 -0300

    finaliando projeto 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +12,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+  alertButtons = ['OK'];
+  ValorCarro = 0;
+  valorRodados = 0;
+  valorFinal = 0;
+  carro='';
+
+
+  valorViagem() {
+this.valorFinal=this.valorRodados * this.ValorCarro
+
+
+switch(this.carro){
+  case 'normal':
+    this.valorFinal = this.valorRodados * 2.50 + 5
+    break;
+  case 'superior':
+    this.valorFinal = this.valorRodados * 3.50 + 7.50;
+    break;
+    case 'vip':
+      this.valorFinal = this.valorRodados * 5.50 + 10;
+      break;
+}
+
+
+
+
+
+
+
+    if (this.valorFinal > 150) {
+      this.valorFinal = this.valorFinal * (25 / 100);
+    } else {
+      this.valorFinal = this.valorFinal * (20 / 100);
+    }
+
+
+
+
+  }
 
 }
+
